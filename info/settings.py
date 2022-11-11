24214682d56828778584856cc3df7a7399a9dad1
brice <wagaflotte@gmail.com>
2022-11-11 12:51:46 +0000

    addres github et mot passe effacer

--- info/settings.py
+++ info/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='wagaflotte@gmail.com'
-EMAIL_HOST_PASSWORD = 'first7538'
+EMAIL_HOST_USER ='your email here'
+EMAIL_HOST_PASSWORD = 'your password'
